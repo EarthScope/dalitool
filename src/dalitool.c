b3c6abf8e2fda09e3ef506e41abb0af2ef08b0c8
Chad Trabant <chad@iris.washington.edu>
2011-01-04 08:04:54 +0000

    *** empty log message ***

--- src/dalitool.c
+++ src/dalitool.c
@@ -23,7 +23,7 @@
 #include "dalixml.h"
 
 #define PACKAGE   "dalitool"
-#define VERSION   "2010.027"
+#define VERSION   "2011.003"
 
 static char verbose        = 0;     /* Flag to control general verbosity */
 static char ppackets       = 0;     /* Flag to control printing of data packets */
