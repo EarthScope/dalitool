8c2a62aa473c7c28fc8b01a524ece8adbf1f456a
Chad Trabant <chad@iris.washington.edu>
2013-10-07 17:39:46 +0000

    add time.h include for linux

--- src/dalitool.c
+++ src/dalitool.c
@@ -26,7 +26,7 @@
 #include "dalixml.h"
 
 #define PACKAGE   "dalitool"
-#define VERSION   "2013.277"
+#define VERSION   "2013.280"
 
 static char verbose        = 0;     /* Flag to control general verbosity */
 static char console        = 0;     /* Flag to control interactive console session */
--- src/dlconsole.c
+++ src/dlconsole.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <errno.h>
 
 #include <libmseed.h>
