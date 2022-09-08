563533c688b4636d098594632b6c8afd0a6f69d9
chad-iris <chad.trabant@iris.edu>
2022-09-08 10:44:29 -0700

    update README -> .md and add md manual

--- README.md
+++ README.md
@@ -1,37 +1,33 @@
 
-dalitool - a general use DataLink tool.
+# dalitool - a general use DataLink tool.
 
-For usage information see the dalitool(1) man page in the 'doc'
+For usage information see the [dalitool manual](doc/dalitool.md) in the 'doc'
 directory.
 
--- Building and Installation --
+## Building and Installation
 
 In most environments a simple 'make' will compile the program.
  
-Using GCC under Linux or Solaris, running 'make static' will compile a
-static version if the appropriate libraries are present.
- 
-GCC can be explicitly used by running 'make gcc'.
-
 SunOS/Solaris:
  
 In order to compile under Solaris the 'src/Makefile' needs to be edited.
 See the Makefile for instructions.
  
- 
 For further installation simply copy the resulting binary and man page
 (in the 'doc' directory) to appropriate system directories.
 
--- Licensing --
+## License
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or (at
-your option) any later version.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License (GNU-GPL) for more details.  The GNU-GPL and
-further information can be found here: http://www.gnu.org/
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Copyright (C) 2020 Chad Trabant, IRIS Data Management Center
--- src/common.c
+++ src/common.c
@@ -1,16 +1,14 @@
 /***************************************************************************
  * common.c
- * 
- * Common routines.
  *
- * modified 2013.217
+ * Common routines.
  ***************************************************************************/
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <errno.h>
 
 #include <libdali.h>
 #include <libmseed.h>
@@ -19,7 +17,6 @@
 #include "dalixml.h"
 
 static void msr_print_samples (MSRecord *msr, int psamples);
-
 
 /***************************************************************************
  * packet_handler:
@@ -38,72 +35,71 @@ static void msr_print_samples (MSRecord *msr, int psamples);
  ***************************************************************************/
 int
 packet_handler (DLPacket *dlpacket, void *packetdata,
-		int ppackets, int psamples, FILE *outfp)
+                int ppackets, int psamples, FILE *outfp)
 {
   dltime_t now;
   char timestr[60];
   char type[10];
   int rv;
-  
-  now = dlp_time();
-  
+
+  now = dlp_time ();
+
   /* Print basic packet details */
   dl_dltime2seedtimestr (dlpacket->datastart, timestr, 1);
   dl_log (0, 0, "%s (%lld), %s, %d (data: %.1f sec, feed: %.1f sec)\n",
-	  dlpacket->streamid, dlpacket->pktid, timestr, dlpacket->datasize,
-	  ((double)(now - dlpacket->dataend) / DLTMODULUS),
-	  ((double)(now - dlpacket->pkttime) / DLTMODULUS));
-  
-  /* Print packet and sample details if requested */
-  if ( ppackets > 0 )
-    {
-      /* Parse packet type from stream ID */
-      if ( dl_splitstreamid (dlpacket->streamid, NULL, NULL, NULL, NULL, type) )
-	{
-	  dl_log (2, 0, "Error parsing stream ID: %s\n", dlpacket->streamid);
-	  return -1;
-	}
-      
-      /* Handle MSEED packet types */
-      if ( ! strncmp (type, "MSEED", sizeof(type)) )
-	{
-	  MSRecord *msr = 0;
-	  
-	  rv = msr_unpack (packetdata, dlpacket->datasize, &msr, psamples, 0);
-	  
-	  if ( rv != MS_NOERROR )
-	    {
-	      dl_log (2, 0, "Cannot parse Mini-SEED record: %s\n", ms_errorstr(rv));
-	    }
-	  else
-	    {
-	      /* Print more packet details */
-	      msr_print (msr, ppackets-1);
-	      
-	      if ( psamples )
-		msr_print_samples (msr, psamples);
-	    }
-	  
-	  msr_free (&msr);
-	}
-      /* This would be the place to add support for other packet types */
-      /* Otherwise this is an unrecognized packet type */
-      else
-	{
-	  dl_log (2, 0, "Unrecognized packet type: %s\n", type);
-	}
-    }
-  
-  /* Write packet to dumpfile if defined */
-  if ( outfp )
-    {
-      if ( fwrite (packetdata, dlpacket->datasize, 1, outfp) == 0 )
-	dl_log (2, 0, "fwrite(): error writing packet data to output file\n");
-    }
-  
-  return 0;
-}  /* End of packet_handler() */
+          dlpacket->streamid, dlpacket->pktid, timestr, dlpacket->datasize,
+          ((double)(now - dlpacket->dataend) / DLTMODULUS),
+          ((double)(now - dlpacket->pkttime) / DLTMODULUS));
 
+  /* Print packet and sample details if requested */
+  if (ppackets > 0)
+  {
+    /* Parse packet type from stream ID */
+    if (dl_splitstreamid (dlpacket->streamid, NULL, NULL, NULL, NULL, type))
+    {
+      dl_log (2, 0, "Error parsing stream ID: %s\n", dlpacket->streamid);
+      return -1;
+    }
+
+    /* Handle MSEED packet types */
+    if (!strncmp (type, "MSEED", sizeof (type)))
+    {
+      MSRecord *msr = 0;
+
+      rv = msr_unpack (packetdata, dlpacket->datasize, &msr, psamples, 0);
+
+      if (rv != MS_NOERROR)
+      {
+        dl_log (2, 0, "Cannot parse Mini-SEED record: %s\n", ms_errorstr (rv));
+      }
+      else
+      {
+        /* Print more packet details */
+        msr_print (msr, ppackets - 1);
+
+        if (psamples)
+          msr_print_samples (msr, psamples);
+      }
+
+      msr_free (&msr);
+    }
+    /* This would be the place to add support for other packet types */
+    /* Otherwise this is an unrecognized packet type */
+    else
+    {
+      dl_log (2, 0, "Unrecognized packet type: %s\n", type);
+    }
+  }
+
+  /* Write packet to dumpfile if defined */
+  if (outfp)
+  {
+    if (fwrite (packetdata, dlpacket->datasize, 1, outfp) == 0)
+      dl_log (2, 0, "fwrite(): error writing packet data to output file\n");
+  }
+
+  return 0;
+} /* End of packet_handler() */
 
 /***************************************************************************
  * info_handler:
@@ -116,43 +112,42 @@ int
 info_handler (char *infotype, char *infodata, int infolen, int formatlevel)
 {
   ezxml_t xmldoc;
-  
-  /* Parse XML string into EzXML representation */
-  if ( (xmldoc = ezxml_parse_str (infodata, infolen)) == NULL )
-    {
-      dl_log (2, 0, "info_handler(): XML parse error\n");
-      
-      return -1;
-    }
-  
-  /* Print formatted information */
-  if ( ! strncasecmp (infotype, "STATUS", 6) )
-    {
-      prtinfo_status (xmldoc, formatlevel, stdout);
-    }
-  else if ( ! strncasecmp (infotype, "CONNECTIONS", 11) )
-    {
-      prtinfo_connections (xmldoc, formatlevel, stdout);
-    }
-  else if ( ! strncasecmp (infotype, "STREAMS", 7) )
-    {
-      prtinfo_streams (xmldoc, formatlevel, stdout);
-    }
-  else
-    {
-      dl_log (2, 0, "info_handler(): unrecognized INFO type: %s\n", infotype);
-    }
-  
-  ezxml_free (xmldoc);
-  
-  return 0;
-}  /* End of info_handler() */
 
+  /* Parse XML string into EzXML representation */
+  if ((xmldoc = ezxml_parse_str (infodata, infolen)) == NULL)
+  {
+    dl_log (2, 0, "info_handler(): XML parse error\n");
+
+    return -1;
+  }
+
+  /* Print formatted information */
+  if (!strncasecmp (infotype, "STATUS", 6))
+  {
+    prtinfo_status (xmldoc, formatlevel, stdout);
+  }
+  else if (!strncasecmp (infotype, "CONNECTIONS", 11))
+  {
+    prtinfo_connections (xmldoc, formatlevel, stdout);
+  }
+  else if (!strncasecmp (infotype, "STREAMS", 7))
+  {
+    prtinfo_streams (xmldoc, formatlevel, stdout);
+  }
+  else
+  {
+    dl_log (2, 0, "info_handler(): unrecognized INFO type: %s\n", infotype);
+  }
+
+  ezxml_free (xmldoc);
+
+  return 0;
+} /* End of info_handler() */
 
 /***************************************************************************
- * print_samples:
+ * msr_print_samples:
  *
- * Print samples in the supplied MSRecord with a simple format.
+ * Print samples in the MSRecord with a simple format.
  ***************************************************************************/
 static void
 msr_print_samples (MSRecord *msr, int psamples)
@@ -160,41 +155,41 @@ msr_print_samples (MSRecord *msr, int psamples)
   int line, col, cnt, samplesize;
   int lines = (msr->numsamples / 6) + 1;
   void *sptr;
-  
-  if ( (samplesize = ms_samplesize(msr->sampletype)) == 0 )
-    {
-      dl_log (2, 0, "Unrecognized sample type: %c\n", msr->sampletype);
-    }
-  
-  if ( msr->sampletype == 'a' )
-    dl_log (0, 0, "ASCII Data:\n%.*s\n", msr->numsamples, (char *)msr->datasamples);
+
+  if ((samplesize = ms_samplesize (msr->sampletype)) == 0)
+  {
+    dl_log (2, 0, "Unrecognized sample type: %c\n", msr->sampletype);
+  }
+
+  if (msr->sampletype == 'a')
+    dl_log (0, 0, "ASCII Data:\n%.*s\n", (int)msr->numsamples, (char *)msr->datasamples);
   else
-    for ( cnt = 0, line = 0; line < lines; line++ )
+    for (cnt = 0, line = 0; line < lines; line++)
+    {
+      for (col = 0; col < 6; col++)
       {
-	for ( col = 0; col < 6 ; col ++ )
-	  {
-	    if ( cnt < msr->numsamples )
-	      {
-		sptr = (char*)msr->datasamples + (cnt * samplesize);
-		
-		if ( msr->sampletype == 'i' )
-		  dl_log (0, 0, "%10d  ", *(int32_t *)sptr);
-                
-		else if ( msr->sampletype == 'f' )
-		  dl_log (0, 0, "%10.8g  ", *(float *)sptr);
-		
-		else if ( msr->sampletype == 'd' )
-		  dl_log (0, 0, "%10.10g  ", *(double *)sptr);
-		
-		cnt++;
-	      }
-	  }
-	dl_log (0, 0, "\n");
-	
-	/* If only printing the first 6 samples break out here */
-	if ( psamples == 1 )
-	  break;
+        if (cnt < msr->numsamples)
+        {
+          sptr = (char *)msr->datasamples + (cnt * samplesize);
+
+          if (msr->sampletype == 'i')
+            dl_log (0, 0, "%10d  ", *(int32_t *)sptr);
+
+          else if (msr->sampletype == 'f')
+            dl_log (0, 0, "%10.8g  ", *(float *)sptr);
+
+          else if (msr->sampletype == 'd')
+            dl_log (0, 0, "%10.10g  ", *(double *)sptr);
+
+          cnt++;
+        }
       }
-  
+      dl_log (0, 0, "\n");
+
+      /* If only printing the first 6 samples break out here */
+      if (psamples == 1)
+        break;
+    }
+
   return;
-}  /* End of msr_print_samples() */
+} /* End of msr_print_samples() */
